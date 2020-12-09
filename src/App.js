63547158084e3a4f51ebbb1a67e36c4ebccf4521
michelle <michelle@michelles-MacBook-Pro.local>
2020-12-08 22:08:30 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import Subscribers from './components/subscribers';
+import SubscriberDetail from './components/subscriber_detail';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
+
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/subscribers" children={<Subscribers />} />
+          <Route exact path="/subscribers/:id" children={<SubscriberDetail />} />
+        </Switch>
+      </Router>
+    )
+  }
+
+}
+
+export default App;
